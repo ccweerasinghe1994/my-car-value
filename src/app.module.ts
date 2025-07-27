5a928fb994bd07a4fddf1cd4cfca8f9664b2323c
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2025-07-27 08:19:26 +0530

    fix: correct import order for UsersModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule, ReportsModule],
